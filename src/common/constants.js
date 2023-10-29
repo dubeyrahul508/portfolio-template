c21c20dddd4e0086979588b540031cf154839a40
Rahul Dubey <rahuldubey@rahuls-mbp.home>
2023-10-29 13:21:03 +0000

    first commit

--- src/common/constants.js
+++ src/common/constants.js
@@ -10,13 +10,13 @@ const CONTACTS = {
 };
 
 const METADATA = {
-  TITLE: 'Kamolesh Mondal: Mobile & Web developer',
+  TITLE: 'Rahul Dubey: Mobile & Web developer',
   DESCRIPTION: '',
 };
 
-const RESUME_FILE_NAME = 'Kamolesh_Mondal_Resume.pdf';
+const RESUME_FILE_NAME = 'Rahul_Dubey_Resume.pdf';
 const PROFILE_IMAGE_NAME = 'profile.jpg';
-const DEVELOPER_NAME = 'Kamolesh Mondal';
+const DEVELOPER_NAME = 'Rahul Dubey';
 const DESIGNATION = 'Full Stack Mobile &\nWeb Developer';
 const INTRO =
   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
@@ -27,29 +27,29 @@ const INFO = [
   },
   {
     label: 'Projects completed',
-    value: '15+',
+    value: '10+',
   },
 ];
 const URLS = [
   {
     for: APPS.GITHUB,
-    url: 'https://github.com/kamolesh97',
+    url: 'https://github.com/dubeyrahul508',
   },
   {
     for: APPS.LINKEDIN,
-    url: 'https://www.linkedin.com/in/kamoleshmondal/',
+    url: 'https://www.linkedin.com/in/dubey5864/',
   },
   {
     for: CONTACTS.EMAIL,
-    url: 'mailto:mondalkamolesh34@gmail.com',
+    url: 'mailto:dubey5864@gmail.com',
   },
   {
     for: CONTACTS.PHONE,
-    url: 'tel:+918169088461',
+    url: 'tel:+447553729181',
   },
   {
     for: CONTACTS.WHATSAPP,
-    url: ' https://wa.me/8169088461',
+    url: ' https://wa.me/447553729181',
   },
 ];
 
